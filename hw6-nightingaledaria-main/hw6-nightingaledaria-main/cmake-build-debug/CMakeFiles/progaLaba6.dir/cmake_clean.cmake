13a3770cfbe6f2818beec94bf0b3b673fe36fc70
nightingaledaria <nightingale.daria@mail.ru>
2022-01-23 23:02:56 +0300

    6th lab is committed

---
+++ hw6-nightingaledaria-main/hw6-nightingaledaria-main/cmake-build-debug/CMakeFiles/progaLaba6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progaLaba6.dir/main.c.obj"
+  "libprogaLaba6.dll.a"
+  "progaLaba6.exe"
+  "progaLaba6.exe.manifest"
+  "progaLaba6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/progaLaba6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
